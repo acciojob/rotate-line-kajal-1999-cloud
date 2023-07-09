f45015c7827695b0fec8c9d7ccdb18e7156ce5ee
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-07-09 17:12:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 //your JS code here. If required.
 const line = document.getElementById('line') ;
-const rotationAngle = 0 ;
+var rotationAngle = 0 ;
 
 function rotateLine() {
 	rotationAngle += 1 ;
-    line.style.transform = `rotate ${rotationAngle}deg` ;
+    line.style.transform = `rotate(${rotationAngle}deg)` ;
 
 	// calling the rotation line function again
 	requestAnimationFrame(rotateLine) ;
 }
 rotateLine() ;
+
